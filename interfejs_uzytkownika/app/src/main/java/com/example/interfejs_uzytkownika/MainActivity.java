eb814a84a282cfe1a3c476ceb51b958068426158
Bartosz Wiaderny <10376794u@qed.local>
2024-12-05 14:55:04 +0100

    dod

--- interfejs_uzytkownika/app/src/main/java/com/example/interfejs_uzytkownika/MainActivity.java
+++ interfejs_uzytkownika/app/src/main/java/com/example/interfejs_uzytkownika/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.interfejs_uzytkownika;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     private Button delete;
     private TextView polubienia;
     private int ilosc_polubien=0;
+
+    private Button zapisz;
+    private Button pokaz;
+
+    private EditText emial;
+    private EditText pass;
+    private EditText r_pass;
+    private TextView info;
+    private String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
         like=findViewById(R.id.like);
         delete=findViewById(R.id.delete);
         polubienia=findViewById(R.id.polubienia);
+
+        emial=findViewById(R.id.email);
+        pass=findViewById(R.id.pass);
+        r_pass=findViewById(R.id.r_pass);
+        info=findViewById(R.id.info);
+
+        zapisz=findViewById(R.id.zapisz);
+        pokaz=findViewById(R.id.show_user);
+
 
         like.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +60,48 @@ public class MainActivity extends AppCompatActivity {
                     ilosc_polubien=0;
                     polubienia.setText(String.valueOf(ilosc_polubien) +" polubień");
                 }
+            }
+        });
+
+        zapisz.setOnClickListener(new View.setOnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String email_input=emial.getText().toString().trim();
+                String pass_input=pass.getText().toString().trim();
+                String r_pass_input=r_pass.getText().toString().trim();
+
+                if(email_input.isEmpty()){
+                    info.setText("pole email nie moze byc puste");
+                    return;
+                }
+                if(pass_input.isEmpty()){
+                    info.setText("pole haslo nie moze byc puste");
+                    return;
+                }
+                if(r_pass_input.isEmpty()){
+                    info.setText("pole Powtorz haslo nie moze byc puste");
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email_input).matches()){
+                    info.setText("nie poprawny zapis maila");
+                    return;
+                }
+                if(!r_pass_input.equals(pass_input)){
+                    info.setText("haslo musi sie powtarzac");
+                    return;
+                }
+                if(!email_input.isEmpty() && !pass_input.isEmpty() && !r_pass_input.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email_input).matches() && r_pass_input.equals(pass_input)){
+                    user="";
+                    info.setText("Zarejestrowano "+email_input);
+                    user=email_input;
+                }
+            }
+        });
+
+        pokaz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText("ostatnio zarejestrowany emial to "+user);
             }
         });
 
